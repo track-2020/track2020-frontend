86d1bc5b1520af1f8776e5f5fb474cce4b963ac7
Ben Parisot <me@benparisot.com>
2019-06-05 15:23:18 -0700

    set up folder structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <h1>Track 2020, baby!</h1>
+  );
+}
