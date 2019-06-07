9586266486096dc09c91fa019d00818ebabf36fb
Ben Parisot <me@benparisot.com>
2019-06-06 21:09:26 -0700

    write candidate reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import login from './loginReducer';
 import signup from './signUpReducer';
 import issues from './issuesReducer';
+import candidates from './candidateReducer';
 
 export default combineReducers({
   login,
   signup,
-  issues
+  issues,
+  candidates
 });
