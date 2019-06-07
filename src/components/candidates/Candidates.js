421d9f6d7479b7f856be4dd8d36bfcd9c8d79c5c
Ben Parisot <me@benparisot.com>
2019-06-07 09:19:52 -0700

    WIP rewrite candidates as class component

--- src/components/candidates/Candidates.js
+++ src/components/candidates/Candidates.js
@@ -1,28 +1,61 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Candidate from './Candidate';
 
-function Candidates({ candidates }) {
-  const candidateList = candidates.map((candidate, index) => {
-    return <li key={index}>
-      <Candidate image={candidate.image} name={candidate.name} score={candidate.score} bio={candidate.bio} />
-    </li>;
-  });
+export default class Candidates extends PureComponent {
+  static propTypes = {
+    candidates: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      score: PropTypes.number.isRequired,
+      issue0Score: PropTypes.shape({
+        issue: PropTypes.string.isRequired,
+        score: PropTypes.number.isRequired
+      }).isRequired,
+      issue1Score: PropTypes.shape({
+        issue: PropTypes.string.isRequired,
+        score: PropTypes.number.isRequired
+      }).isRequired,
+      issue2Score: PropTypes.shape({
+        issue: PropTypes.string.isRequired,
+        score: PropTypes.number.isRequired
+      }).isRequired,
+      issue3Score: PropTypes.shape({
+        issue: PropTypes.string.isRequired,
+        score: PropTypes.number.isRequired
+      }).isRequired,
+      issue4Score: PropTypes.shape({
+        issue: PropTypes.string.isRequired,
+        score: PropTypes.number.isRequired
+      }).isRequired,
+      description: PropTypes.string.isRequired,
+    })).isRequired,
+    fetch: PropTypes.func.isRequired,
+    loading: PropTypes.func.isRequired,
+    error: PropTypes.func.isRequired,
+    userId: PropTypes.number.isRequired
+  }
 
-  return (
-    <ul>
-      {candidateList}
-    </ul>
-  );
+  fetchCandidates = (userId) => {
+    this.props.fetch(userId);
+  }
+
+  componentDidMount() {
+    this.fetchCandidates(this.props.userId);
+  }
+
+  render() {
+    const candidateList = this.props.candidates.map((candidate) => {
+      return <li key={candidate.userId}>
+        <Candidate image={candidate.image} name={candidate.name} score={candidate.score} bio={candidate.bio} />
+      </li>;
+    });
+
+    return (
+      <ul>
+        {candidateList}
+      </ul>
+    );
+  }
 }
 
-Candidates.propTypes = {
-  candidates: PropTypes.arrayOf(PropTypes.shape({
-    image: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    score: PropTypes.number.isRequired,
-    bio: PropTypes.string.isRequired
-  })).isRequired
-};
-
-export default Candidates;
